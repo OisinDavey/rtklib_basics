d2ea9b39497882bd41d7ae99fb5e5cfa919936f8
OisinDavey <oisindavey02@gmail.com>
2022-06-23 09:59:41 +0100

    Made scripts more easily changed for use, added comments, fixed bugs (malredirects)

--- start_base_station.sh
+++ start_base_station.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+
+#set to true if both rover & base station are connected to the one pi
+UNITEDDEVICE=1
+
 TS=`date +%Y%m%d-%H%M`
 STR2STR=str2str
+GNSSRTKLOC=../../..
+LOGFOLDER=logs_for_modified_scripts
+
 DEVICE=ttyACM$1
+
 SURVEY_ACCURACY_M=10
 SURVEY_MIN_TIME_S=120
 
 if [ "$#" -ne 1 ]; then
-        echo "./start_base_station <device-port-number>"
-        exit 1
+    echo "./start_base_station <device-port-number>"
+    exit 1
 fi
 
-#echo ${DEVICE}
+LOCALHOST_RAWOUT=21104
+LOCALHOST_RTCM3OUT=21101
 
-../src/ubx/ubx_cfg_tmode3 $SURVEY_MIN_TIME_S $SURVEY_ACCURACY_M -b > /dev/${DEVICE}
+if [ ${UNITEDDEVICE} -eq 1 ]; then
+    echo "Running as though rover & station are on the same pi"
+else
+    echo "Running as though rover & station are on different pis"
+    LOCALHOST_RAWOUT=21102
+    LOCALHOST_RTCM3OUT=21101
+fi
 
-echo "Starting raw data streaming on port 21104"
-${STR2STR} -in serial://${DEVICE}:230400#ubx -out tcpsvr://:21104 -c ./cfg_f9p_basestation.cmd >& base-${TS}.log &
+${GNSSRTKLOC}/src/ubx/ubx_cfg_tmode3 $SURVEY_MIN_TIME_S $SURVEY_ACCURACY_M -b > /dev/${DEVICE}
+
+echo "Starting raw data streaming on port ${LOCALHOST_RAWOUT}"
+${STR2STR} -in serial://${DEVICE}:230400#ubx -out tcpsvr://:${LOCALHOST_RAWOUT} -c ./${GNSSRTKLOC}/cfg_f9p_basestation.cmd >& ${LOGFOLDER}/base-${TS}.log &
 sleep 1
-netcat localhost 21102 > base-${TS}.ubx &
+netcat localhost ${LOCALHOST_RAWOUT} > ${LOGFOLDER}/base-${TS}.ubx &
 
-# Make RTCM3 available on port 21101
-echo "Starting RTCM3 server on port 21101"
-netcat localhost 21104 | ./../src/rtcm3_filter | ${STR2STR} -out tcpsvr://:21101 >& base-rtcm-${TS}.log &
+# Make RTCM3 available on port ${LOCALHOST_RTCM3OUT}
+echo "Starting RTCM3 server on port ${LOCALHOST_RTCM3OUT}"
+netcat localhost ${LOCALHOST_RTCM3OUT} | ./${GNSSRTKLOC}/src/rtcm3_filter | ${STR2STR} -out tcpsvr://:${LOCALHOST_RTCM3OUT} >& ${LOGFOLDER}/base-rtcm-${TS}.log &
--- start_rover.sh
+++ start_rover.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 
+#set this to true if both rover & station are connected to the one pi
+UNITEDDEVICE=1
+
 TS=`date +%Y%m%d-%H%M`
-#STR2STR=/home/pi/RTKLIB/app/consapp/str2str/gcc/str2str
 STR2STR=str2str
+GNSSRTKLOC=../../..
+LOGFOLDER=logs_for_modified_scripts
 
 BASE=$1
 DEVICE=ttyACM$2
-LOGFOLDER=logs_for_modified_scripts
 
 if [ "$#" -ne 2 ]; then
-        echo "./start_rover.sh <base-station-ip> <device-port-number> (usually 0)"
+        echo "./start_rover.sh <base-station-ip> <device-port-number>"
         exit 1
 fi
 
-echo ${DEVICE}
+BASESTATION_RTC3OUT=21101
+LOCALHOST_OBSVOUT=21102
+LOCALHOST_PVTOUT=21103
+
+if [ ${UNITEDDEVICE} -eq 1 ]; then
+    echo "Running as though rover & base station are on the same pi"
+else
+    echo "Running as though rover & base station are on different pis";
+    BASESTATION_RTC3OUT=21101
+fi
 
 # Read RTCM from base station and send to GNSS receiver
 echo "Piping RTCM into rover GNSS receiver"
-${STR2STR} -in tcpcli://${BASE}:21101 -out file://dev/${DEVICE} >& ${LOGFOLDER}/rover-rtcm-in-${TS}.log &
+${STR2STR} -in tcpcli://${BASE}:BASESTATION_RTC3OUT -out file://dev/${DEVICE} >& ${LOGFOLDER}/rover-rtcm-in-${TS}.log &
 sleep 2
 
 # Make roving GNSS data available
-echo "Publishing rover GNSS observations on port 21102"
-${STR2STR} -in serial://${DEVICE}:230400 -out tcpsvr://:21102 -c ./cfg_f9p_rover.cmd >& ${LOGFOLDER}/rover-tcpsrv-${TS}.log &
+echo "Publishing rover GNSS observations on port ${LOCALHOST_OBSVOUT}"
+${STR2STR} -in serial://${DEVICE}:230400 -out tcpsvr://:${LOCALHOST_OBSVOUT} -c ./${GNSSRTKLOC}/ublox_f9p/cfg_f9p_rover.cmd >& ${LOGFOLDER}/rover-tcpsrv-${TS}.log &
 sleep 2
 
-# PVT messages only on 21103 (for bandwidth efficiency)
-echo "Publishing rover PVT on port 21103"
-netcat localhost 21102 |  ../src/ubx/ubx_nav_pvt | ${STR2STR} -out tcpsvr://:21103 >& ${LOGFOLDER}/rover-PVT-out-${TS}.log &
+# Make PVT stream available
+echo "Publishing rover PVT on port ${LOCALHOST_PVTOUT}"
+netcat localhost ${LOCALHOST_OBSVOUT} |  ${GNSSRTKLOC}/src/ubx/ubx_nav_pvt | ${STR2STR} -out tcpsvr://:${LOCALHOST_PVTOUT} >& ${LOGFOLDER}/rover-PVT-out-${TS}.log &
 
 # Channel rover NMEA to Raspberry Pi Sensehat LED blinker
-#netcat localhost 21102 | /home/pi/GNSS_RTK/ublox_m8p/sense_hat_indicator >& ${LOGFOLDER}/rover-blink-${TS}.log &
+#netcat localhost ${LOCALHOST_OBSVOUT} | ${GNSSRTKLOC}/ublox_m8p/sense_hat_indicator >& ${LOGFOLDER}/rover-blink-${TS}.log &
 
-netcat localhost 21102 > ${LOGFOLDER}/rover-${TS}.ubx &
-
+#netcat localhost ${LOCALHOST_OBSVOUT} > ${LOGFOLDER}/rover-${TS}.ubx &
 
 # PVT service
-#netcat localhost 21102 | ../src/ubx/ubx_nav_pvt | ${STR2STR} -out tcpsvr://:21103
+#netcat localhost ${LOCALHOST_OBSVOUT} | ${GNSSRTKLOC}/src/ubx/ubx_nav_pvt | ${STR2STR} -out tcpsvr://:${LOCALHOST_PVTOUT}
 
 # Gamepad
-#../src/util/gamepad_events /dev/input/event0 | ${STR2STR} -out tcpsvr://:21104
+#${GNSSRTKLOC}/src/util/gamepad_events /dev/input/event0 | ${STR2STR} -out tcpsvr://:21104
